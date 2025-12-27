0f1918ebd9cf1ae4f429b53edd43645b30677042
baris-ux <baris.ock.etudes@gmail.com>
2025-12-28 00:38:58 +0100

    modification main

--- app/src/main/java/com/example/pdf_maker/MainActivity.kt
+++ app/src/main/java/com/example/pdf_maker/MainActivity.kt
@@ -3,45 +3,33 @@ package com.example.pdf_maker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pdf_maker.ui.theme.Pdf_makerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Pdf_makerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HelloScreen()
         }
     }
-} // commentaire
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Pdf_makerTheme {
-        Greeting("Android")
+fun HelloScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello 👋",
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
